bf4cd106e6dfe89bec5087b1038833e514cb9c3c
NPGonzalez <naypili@gmail.com>
2020-09-25 01:55:31 -0300

    Se validan campos del formulario

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ const showError = (input, message) => {
     small.innerText = message;
 };
 
-const showsucces = (input, message) => {
+const showSuccess = (input, message) => {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
-    // const small = formControl.querySelector('small');
-    // small.innerText = message;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
 };
 
 form.addEventListener('submit', (e) => {
@@ -27,4 +27,34 @@ form.addEventListener('submit', (e) => {
     } else {
         showSuccess(username);
     }
+
+    if (email.value === '') {
+        showError(email, 'Ingrese mail');
+    } else if (isValidemail(email.value)) {
+        showError(email, 'El email no es válido')
+    } else {
+        showSuccess(email);
+    }
+
+    if (password1.value === '') {
+        showError(password1, 'Ingrese contraseña');
+    } else {
+        showSuccess(password1);
+    }
+
+    if (password2.value === '') {
+        showError(password2, 'Ingrese contraseña');
+    } else {
+        showSuccess(password2);
+    }
+
+    isValidemail = (email) => {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    };
+
+
+
+
+
 });
